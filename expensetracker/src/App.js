7cbe11c82807fe8c4d9edf0cb359c992966d87cb
Yashas.C <Yashas.Ramesh@neudesic.com>
2022-10-07 22:31:21 +0530

    Added ChartBar Filter

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -4,33 +4,44 @@ import Expense from "./components/ExpenseItem";
 import data from "./data.js";
 import NewExpense from "./components/NewExpense/NewExpense";
 import ExpenseFilter from "./components/ExpenseFilter";
+import ExpensesChart from './components/ExpenseChart';
 function App() {
-  const [filterYear,setFilterYear]=useState('2020');
-  const [expenseFile,setExpenseFile]=useState(data);
+  const [filterYear, setFilterYear] = useState("2020");
+  const [expenseFile, setExpenseFile] = useState(data);
 
-  const expensedata = expenseFile.map((item) => {
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
+  const filteredExpenses = expenseFile.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
+  console.log(filteredExpenses);
+
+  const addExpenseHandler = (expense) => {
+    setExpenseFile((prevExpenseFile) => {
+      return [expense, ...prevExpenseFile];
+    });
+  };
+
+  let noDataOnFilter=<h2 className="noDataFilter">No data found.</h2>
+
+  const expensedata = filteredExpenses.map((item) => {
     return <Expense key={item.id} {...item} />;
   });
 
-  const filterChangeHandler=selectedYear=>{
-    
-    setFilterYear(selectedYear);
-
-  };
-
-  const addExpenseHandler = (expense) => {
-    setExpenseFile(prevExpenseFile=>{
-      return [expense,...prevExpenseFile];
-      
-    });
-  };
-  console.log(expenseFile);
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      
       <div className="expenses">
-      <ExpenseFilter selectedYear={filterYear} onFilterChange={filterChangeHandler}/>
+        
+        <ExpenseFilter
+          selectedYear={filterYear}
+          onFilterChange={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses}/>
+        {filteredExpenses.length===0 && noDataOnFilter}
         {expensedata}
       </div>
     </div>
--- expensetracker/src/components/NewExpense/NewExpense.js
+++ expensetracker/src/components/NewExpense/NewExpense.js
@@ -1,15 +1,33 @@
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
-const NewExpense=(props)=>{
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-          ...enteredExpenseData
-        };
-        props.onAddExpense(expenseData);
-      };
-    return (<div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>);
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new Expense</button>
+      )}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
+    </div>
+  );
 };
 export default NewExpense;
